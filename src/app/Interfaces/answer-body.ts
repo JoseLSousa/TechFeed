66fa15ae4994adda94ac41652252b0e33394f65e
LucasSousa <lucassousasertania@gmail.com>
2025-02-20 14:34:32 -0300

    Add TypeScript interfaces for quiz components: AnswerBody, Option, Question, and QuestionList

---
+++ src/app/Interfaces/answer-body.ts
@@ -0,0 +1,5 @@
+export interface AnswerBody {
+    title: string,
+    description: string
+    imageUrl: string
+}
---
+++ src/app/Interfaces/question-list.ts
@@ -0,0 +1,7 @@
+import { Question } from "./question"
+
+export interface QuestionList {
+    title: string
+    subtitle: string
+    questions: Question[]
+}
